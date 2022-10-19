1efdd2fe14c1c5b876fc9c5944c9b62957d950b0
João Vitor Marques <jvsilvam@outlook.com>
2022-10-19 12:36:53 -0300

    fix list ads

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -2,7 +2,7 @@ import { GameParams } from '../../@types/navigation';
 import { Entypo } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useEffect, useState } from 'react';
-import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
@@ -65,8 +65,21 @@ export function Game() {
           renderItem={({item}) => <DuoCard data={item} onConnect={() => {}} />}
           horizontal
           style={styles.containerList}
-          contentContainerStyle={styles.contentList}
+          contentContainerStyle={[
+            duos.length > 0
+              ? styles.contentList
+              : {
+                  flex: 1,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                },
+          ]}
           showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={() => (
+            <Text style={styles.emptyListText}>
+              Não há anúncios publicados ainda.
+            </Text>
+          )}
         />
       </SafeAreaView>
     </Background>
