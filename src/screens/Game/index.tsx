6eab974313f0effe9bd6ee1cde4b60f20aec687a
João Vitor Marques <jvsilvam@outlook.com>
2022-10-19 12:28:25 -0300

    create game screen

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,20 +1,74 @@
-import {useRoute} from '@react-navigation/native';
-import React from 'react';
-import {View} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
-import {GameParams} from '../../@types/navigation';
+import { GameParams } from '../../@types/navigation';
+import { Entypo } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
-import {Background} from '../../components/Background';
-import {styles} from './styles';
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { Background } from '../../components/Background';
+import { DuoCard, DuoCardProps } from '../../components/DuoCard';
+import { Heading } from '../../components/Heading';
+import { THEME } from '../../theme';
+import { styles } from './styles';
 
 export function Game() {
+  const navigation = useNavigation();
   const route = useRoute();
+
+  const [duos, setDuos] = useState<DuoCardProps[]>([]);
 
   const game = route.params as GameParams;
 
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    fetch(`http://192.168.1.68:3333/api/games/${game.id}/ads`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => setDuos(data));
+  });
+
   return (
     <Background>
-      <SafeAreaView></SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity onPress={handleGoBack}>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image source={logoImg} style={styles.logo} />
+          <View style={styles.right} />
+        </View>
+
+        <Image
+          source={{uri: game.bannerUrl}}
+          style={styles.cover}
+          resizeMode="cover"
+        />
+
+        <Heading title={game.title} subtitle="Conecte-se e começe a jogar!" />
+
+        <FlatList
+          data={duos}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => <DuoCard data={item} onConnect={() => {}} />}
+          horizontal
+          style={styles.containerList}
+          contentContainerStyle={styles.contentList}
+          showsHorizontalScrollIndicator={false}
+        />
+      </SafeAreaView>
     </Background>
   );
 }
